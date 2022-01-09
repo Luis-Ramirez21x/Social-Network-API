2738ef9ee26381f48271c520b3a299cc9f45d6f5
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-01-08 21:16:44 -0500

    creating model files and created schema for user

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const { Schema, model, SchemaTypes } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+          },
+        email:{
+            type: String,
+            required:true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+        //why is there an id property?
+    }
+);
+
+/* virtual to be tested with seeds data
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+})
+*/

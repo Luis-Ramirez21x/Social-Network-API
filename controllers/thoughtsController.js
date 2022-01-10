d76613f827106ee988b4867dc4c9e7932ea2c8be
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-01-09 21:13:15 -0500

    created thought to be updated to user

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -8,7 +8,18 @@ module.exports = {
     },
     createThought(req,res){
         Thoughts.create(req.body)
-                .then((thought) => res.json(thought))
+                .then((thought) => {
+                    return User.findOneAndUpdate(
+                    { userName: req.body.userName },
+                    { $addToSet: { thoughts: thought._id } },
+                    { new: true }
+                    );
+                })
+                .then((user) =>
+                    !user
+                    ? res.status(404).json({ message: 'Thought created, but found no user with that ID' })
+                    : res.json('Created the post 🎉')
+          )
                 .catch((err) => {
                     console.log(err);
                     return res.status(500).json(err);
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const { getThoughts, createThought } = require('../../controllers/thoughtsController');
 
 router.route('/').get(getThoughts).post(createThought);
+//router.route('/:username').post(createThought);
 
 module.exports = router;
